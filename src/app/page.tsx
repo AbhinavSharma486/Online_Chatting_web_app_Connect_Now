47eecf280ba1da1fe5d861de2989bd59e604f0eb
AbhinavSharma486 <abhinavparashar486@gmail.com>
2024-08-12 20:07:17 +0530

    Auth callback and redish setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import ChatLayout from "@/components/chat/ChatLayout";
 import PreferencesTab from "@/components/PreferencesTab";
 import { cookies } from "next/headers";
-import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
 
   const layout = cookies().get("react-resizable-panels:layout");
   const defaultLayout = layout ? JSON.parse(layout.value) : undefined;
+
 
   return (
     <main className="flex h-screen flex-col items-center justify-center p-4 md:px-24 py-32 gap-4">
